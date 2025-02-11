9892c96fa99babaa0f29f59484245d5cf3fa36a2
FabriDamazio <fabridamazio@gmail.com>
2025-02-11 12:06:18 -0300

    Player horizontal mouse movement

--- player/Player.cs
+++ player/Player.cs
@@ -2,41 +2,74 @@ using Godot;
 
 public partial class Player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+    public const float Speed = 5.0f;
+    public const float JumpVelocity = 4.5f;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector3 velocity = Velocity;
+    private Vector2 _mouseMotion = Vector2.Zero;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("jump") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector3 velocity = Velocity;
+        HandleCameraRotation();
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
+        // Add the gravity.
+        if (!IsOnFloor())
+        {
+            velocity += GetGravity() * (float)delta;
+        }
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+        // Handle Jump.
+        if (Input.IsActionJustPressed("jump") && IsOnFloor())
+        {
+            velocity.Y = JumpVelocity;
+        }
+
+        // Get the input direction and handle the movement/deceleration.
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+        if (direction != Vector3.Zero)
+        {
+            velocity.X = direction.X * Speed;
+            velocity.Z = direction.Z * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion)
+        {
+
+            if (Input.MouseMode == Input.MouseModeEnum.Captured)
+            {
+                var mouseEvent = (InputEventMouseMotion)@event;
+                _mouseMotion = -mouseEvent.Relative * 0.001f;
+            }
+        }
+
+        if (@event.IsActionPressed("ui_cancel"))
+        {
+            Input.MouseMode = Input.MouseModeEnum.Visible;
+        }
+
+    }
+
+    private void HandleCameraRotation()
+    {
+        RotateY(_mouseMotion.X);
+        _mouseMotion = Vector2.Zero;
+    }
 }
